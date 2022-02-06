229a6331abb559a9f4d48b24f5672b2a49af0f55
Boris MAURENCE <b.maurence@gmail.com>
2022-02-06 17:20:05 +0100

    feat: database postgresql config

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'aplica-strapi'),
-      user: env('DATABASE_USERNAME', 'Boris'),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USER),
       password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
     },
   },
 });
